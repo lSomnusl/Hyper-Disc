4550d39b800f66b1e6d9024b855480d11a2d0f29
Sazo <104074955+DagobahSazo@users.noreply.github.com>
2022-11-08 13:25:01 -0300

    Movimiento Arreglado YUPIII
    ANIMACIONES POR EL AMOR DE DIOS

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,16 +3,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    bool wasJustClicked = true;
-    bool canMove;
-    Vector2 playerSize;
-
     Rigidbody2D rb;
+    Vector2 startingPosition;
 
     public Transform BoundaryHolder;
 
     Boundary playerBoundary;
 
+    public Collider2D PlayerCollider { get; private set; }
+
+    public PlayerController Controller;
+
+    public int? LockedFingerID { get; set; }
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startingPosition = rb.position;
+        PlayerCollider = GetComponent<Collider2D>();
+
+        playerBoundary = new Boundary(BoundaryHolder.GetChild(0).position.y,
+                                      BoundaryHolder.GetChild(1).position.y,
+                                      BoundaryHolder.GetChild(2).position.x,
+                                      BoundaryHolder.GetChild(3).position.x);
+
+    }
     struct Boundary
     {
         public float Up, Down, Left, Right;
@@ -22,56 +38,26 @@ public class PlayerMovement : MonoBehaviour
             Up = up; Down = down; Left = left; Right = right;
         }
     }
-
-    // Use this for initialization
-    void Start()
+    private void OnEnable()
     {
-        playerSize = GetComponent<SpriteRenderer>().bounds.extents;
-        rb = GetComponent<Rigidbody2D>();
-
-        playerBoundary = new Boundary(BoundaryHolder.GetChild(0).position.y,
-                                      BoundaryHolder.GetChild(1).position.y,
-                                      BoundaryHolder.GetChild(2).position.x,
-                                      BoundaryHolder.GetChild(3).position.x);
-
+        Controller.Players.Add(this);
+    }
+    private void OnDisable()
+    {
+        Controller.Players.Remove(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MoveToPosition(Vector2 position)
     {
-        if (Input.GetMouseButton(0))
-        {
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 clampedMousePos = new Vector2(Mathf.Clamp(position.x, playerBoundary.Left,
+                                                  playerBoundary.Right),
+                                      Mathf.Clamp(position.y, playerBoundary.Down,
+                                                  playerBoundary.Up));
+        rb.MovePosition(clampedMousePos);
+    }
 
-            if (wasJustClicked)
-            {
-                wasJustClicked = false;
-
-                if ((mousePos.x >= transform.position.x && mousePos.x < transform.position.x + playerSize.x ||
-                mousePos.x <= transform.position.x && mousePos.x > transform.position.x - playerSize.x) &&
-                (mousePos.y >= transform.position.y && mousePos.y < transform.position.y + playerSize.y ||
-                mousePos.y <= transform.position.y && mousePos.y > transform.position.y - playerSize.y))
-                {
-                    canMove = true;
-                }
-                else
-                {
-                    canMove = false;
-                }
-            }
-
-            if (canMove)
-            {
-                Vector2 clampedMousePos = new Vector2(Mathf.Clamp(mousePos.x, playerBoundary.Left,
-                                                                  playerBoundary.Right),
-                                                      Mathf.Clamp(mousePos.y, playerBoundary.Down,
-                                                                  playerBoundary.Up));
-                rb.MovePosition(clampedMousePos);
-            }
-        }
-        else
-        {
-            wasJustClicked = true;
-        }
+    public void ResetPosition()
+    {
+        rb.position = startingPosition;
     }
 }
