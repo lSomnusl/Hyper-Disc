f36c6bdd5ed0878d3769da48292dca32b689ae69
Sazo <104074955+DagobahSazo@users.noreply.github.com>
2022-11-15 16:16:01 -0300

    JuegoPresentado

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    void Update()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
